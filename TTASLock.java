eaae708cb49aca224c064f89f518fe1d50c9c987
patrickkaleo <simuyembapatrick250@gmail.com>
2026-09-02 12:54:54 +0200

    - Task 2 implementation

--- TTASLock.java
+++ TTASLock.java
@@ -1,9 +1,17 @@
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class TTASLock implements Lock {
+    private final AtomicBoolean state = new AtomicBoolean(false);
+
     public void lock() {
-        //
+        while (true) { 
+            while(state.get()) {/*Wait until the lock has be released to attempt acquaring it */ }
+            if(state.getAndSet(true)) return;
+        }
     }
 
     public void unlock() {
-        //
+        state.set(false);
     }
 }
